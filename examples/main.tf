1efe589c55306e20bb0c4ac2152fd1c31532ecb8
Kamil Renczewski <kamil.renczewski@codilime.com>
2017-11-30 16:30:31 +0100

    WIP: Handling object refs

--- examples/main.tf
+++ examples/main.tf
@@ -24,5 +24,38 @@ provider "contrail" {
 
 resource "contrail_virtual_network" "spocknet" {
 	name = "spocknet"
-	parent_fq_name = "default-domain:demo"
+	display_name = "spocknet"
+}
+
+resource "contrail_virtual_network_refs" "spockrefs" {
+	uuid = "${contrail_virtual_network.spocknet.id}"
+	network_ipam_refs {
+		to = "${contrail_network_ipam.spock_ipam.id}"
+		attr {
+			ipam_subnets {
+				subnet_name = "spock_subnet"
+				subnet {
+					ip_prefix = "10.10.10.0"
+					ip_prefix_len = 24
+				}
+			}
+		}
+	}
+}
+
+resource "contrail_subnet" "sub_spocknet" {
+	name = "sub_spocknet"
+	subnet_ip_prefix {
+		ip_prefix = "10.10.10.0"
+		ip_prefix_len = 24
+	}
+	display_name = "sub spocknet"
+}
+
+resource "contrail_network_ipam" "spock_ipam" {
+	name = "spock_ipam"
+}
+
+output "spocknet-id" {
+	value = "${contrail_virtual_network.spocknet.id}"
 }
