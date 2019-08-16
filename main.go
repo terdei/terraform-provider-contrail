f7d0f3a51e3dc7936c528df17f833bc49848cc39
Stan da Petrosian <stanislau.petrusiou@Pan-Net.eu>
2019-08-16 11:28:35 +0200

    Fixed main

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-
-	"github.com/terraform-provider-contrail/contrail"
+	"github.com/terraform-provider-contrail/contrail/resources"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return contrail.Provider()
+			return resources.Provider()
 		},
 	})
 }
